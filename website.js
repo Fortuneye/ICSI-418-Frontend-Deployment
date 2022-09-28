0301b9462ee16d3cbf622ed36f5ec6de24dade02
Fortuneye <ipalang17@gmail.com>
2022-09-28 16:56:43 -0400

    Final edits?

--- website.js
+++ website.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+var express = require('express'); //require the express module
+var app = express(); //create express object
+const bodyParser = require("body-parser"); //bodyparser parses an html request body for POST data
+app.use(bodyParser.json()); //use json body parser for the express object
 
 //use ejs as the engine to view our page files
 app.set('view engine', 'ejs');
@@ -9,19 +9,22 @@ app.set('view engine', 'ejs');
 
 // get the main page file
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('index'); //render the page and send it to response
 });
 
 //get adding page
 app.get('/adding.ejs', function(request, response) {
-  response.render('adding');
+  response.render('adding'); //render the page and send it to response
 });
 
+//post request for adding two numbers
 app.post("/add", (request, response) => {
-  const num1 = request.body.num1;
-  const num2 = request.body.num2;
-  const result = arseInt(num1) + parseInt(num2);
-  response.send({
+  const num1 = request.body.num1; //first number
+  const num2 = request.body.num2; //second number
+  const result = parseInt(num1) + parseInt(num2); //add the two and set them to result
+  response.send({ //send all to response
+    num1: num1,
+    num2: num2,
     result: result
   });
 });
